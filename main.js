d3515042404e52d4417bd7af1d1b20f2b1900335
Taylor Phelps <tphelps1993@gmail.com>
2020-12-02 14:36:35 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const Array = require('./array');
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+
+  console.log(arr);
+}
+
+main();
